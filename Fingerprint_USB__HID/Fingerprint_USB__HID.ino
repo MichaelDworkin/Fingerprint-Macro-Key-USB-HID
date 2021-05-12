96e265de0e2c0f64239ee72572af0356a02e0194
Michael Dworkin <52674373+MichaelDworkin@users.noreply.github.com>
2021-05-12 09:04:29 +0200

    Add files via upload

--- Fingerprint_USB__HID/Fingerprint_USB__HID.ino
+++ Fingerprint_USB__HID/Fingerprint_USB__HID.ino
@@ -4,65 +4,74 @@
 // shortcuts key fingerprint
 // https://esp8266-server.de/fingerprint.html
 // Arduino Leonardo Micro + FPC1021A fingerprint ID809
-
-// Befehlformat: "§", Befehl, Wert, Leerzeichen (Beispiel "Wort1§t1000 Wort2" Befehl warte 1 sekunde)
-// §p - Press         "§p0x80 "  Strg drücken
-// §r - Release       "§r0x80 "  Strg loslassen
-// §a - Release All   "§a "      Alle Tasten Loslassen
-// §w - Write         "§w0xB0 "  Enter klicken
-// §t - Timer         "§t3000  " 3 Sekunden abwarten
-// Scancodes Zuordnung bitte hier entnehmen
-// https://github.com/arduino-libraries/Keyboard/blob/master/src/Keyboard.h
 // Die Makros und Texte zum Fingerabdrucken befinden sich im "Makros.h"
+// Bitte vorher das Sketsh "Fingerprint_Einlesen.ino" laden und die Finger im Sensor registrieren.
 
 #include <stdint.h>
 #include <Adafruit_Fingerprint.h>
-#include "KeyboardDE.h"
-#include "Makros.h"
-#define SensorPassword 0
+#include "KeyboardDE.h"           // Speziel für Deutsche Tastaturbelegung
+#include "MakrosPass.h"           // Hier bitte die Makros und Passwörter hinterlegen
 
-#define mySerial Serial1
-#define TouchSensor 2
+#define SensorPassword 0x00000000          // Fingerprint password, falls gesetzt, hier eintragen (4Byte)
 
-Adafruit_Fingerprint finger = Adafruit_Fingerprint(&mySerial,SensorPassword);
+#define TouchSensor 2             // Pin für Fingererkennung
+#define active_low  1             // 0- FPC1021A, 1- R503 
+
+Adafruit_Fingerprint finger = Adafruit_Fingerprint(&Serial1,SensorPassword);
 
 // ---------------- Setup ------------------------------------------
-
 void setup(void)
 {
-  Serial.begin(115200);
-  finger.begin(57600);
+  Serial.begin(115200);         // Kommunikation mit PC Statusmeldungen / Debug Ausgaben
+  finger.begin(57600);          // Kommunikation mit Singerabdrucksensor (57600baud ist standart)
   pinMode(TouchSensor, INPUT); 
-  pinMode(TouchSensor, INPUT); 
-  if (finger.verifyPassword() and Serial)Serial.println("Fingerabdrucksensor angeschlossen");
-  Keyboard.begin();
+  if (finger.verifyPassword())
+  {
+    if (Serial) Serial.println("Fingerabdrucksensor angeschlossen");
+    Keyboard.begin();            // Starte USB HID Tastatur Emulation
+  }
+  else                           // Fingerabdrucksensor antwortet nicht
+  {
+    while(!Serial)               // Warte bis serielle Terminal gestartet wurde
+    Serial.println("Fingerabdrucksensor antwortet nicht");
+    while(1);                    // Programmausführung stoppen 
+  } 
 }
 
 uint8_t id;
 
 void loop(void) 
 { 
-while (digitalRead(TouchSensor))
-  {
-    int FingerID = getFingerprintIDez();
-    if (FingerID>0)KeyOutput(data[FingerID-1]); 
-  }  
+    int FingerID = getFingerprintIDez();             // Erkennung starten
+    if ( FingerID > 0 )
+    {
+    KeyOutput(data[FingerID-1]);   // Wenn erkannt, Keybord ausgabe des Strings aus dem Array auf der ID - Position
+    while (!(active_low ^ digitalRead(TouchSensor))); // hier Bleiben bis neue Berührung
+    }
+    
 }
 
 int getFingerprintIDez() 
 {
   uint8_t p = finger.getImage();
   if (p != FINGERPRINT_OK)  return -1;
-
+  finger.LEDcontrol(FINGERPRINT_LED_ON, 0, FINGERPRINT_LED_PURPLE); // Fingerabdruck eingelesen
   p = finger.image2Tz();
+  finger.LEDcontrol(FINGERPRINT_LED_OFF, 0, FINGERPRINT_LED_PURPLE);
   if (p != FINGERPRINT_OK)  return -1;
 
   p = finger.fingerSearch();
-  if (p != FINGERPRINT_OK)  return -1;
-
+  if (p != FINGERPRINT_OK) 
+  {
+    finger.LEDcontrol(FINGERPRINT_LED_OFF, 0, FINGERPRINT_LED_PURPLE); // Fingerabdruck nicht im Datenbank
+    finger.LEDcontrol(FINGERPRINT_LED_GRADUAL_OFF, 500, FINGERPRINT_LED_RED);
+    return -1;
+  }
+  
+  finger.LEDcontrol(FINGERPRINT_LED_OFF, 0, FINGERPRINT_LED_PURPLE);  // Fingerabdruck im Datenbank gefunden
+  finger.LEDcontrol(FINGERPRINT_LED_GRADUAL_OFF, 200, FINGERPRINT_LED_BLUE);
   return finger.fingerID;
 }
-
 
 
 // ---------------- HEX String in Integer umwandeln  -----------------------
@@ -86,12 +95,11 @@ unsigned int hexToDec(String hexString)
 }
 
 
-
 // ---------- Ausgabe des Textes über HID-Keybord und Ausführung der Befehle -----
 
 void KeyOutput(const String str)
 {
-  int pos = str.indexOf('§');
+  int pos = str.indexOf('§');     
   while (pos >= 0)
   {
     if (pos > 0) Keyboard.print(str.substring(0, pos - 1));
